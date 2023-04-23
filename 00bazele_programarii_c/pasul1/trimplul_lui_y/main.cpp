571dd92fd4305233fe0c96afea941059ce1fd6df
Petrica <41289735+pet5555@users.noreply.github.com>
2023-04-23 18:45:38 +0300

    adaugare problema triplul lui y

---
+++ 00bazele_programarii_c/pasul1/trimplul_lui_y/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//#814.Fiind date două numere naturale x și y determinați
+//valoarea care trebuie adunată la x pentru a obține triplul lui y.
+int main(){
+    int x,y;
+    cin>>x >>y;
+    cout<< y*3-x;
+    return 0;
+}
