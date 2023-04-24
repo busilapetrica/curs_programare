a6c15d2a1e58b6cacaef63cf9ac3b067693df982
Petrica <41289735+pet5555@users.noreply.github.com>
2023-04-24 18:50:47 +0300

    Urare Craciun

---
+++ 00bazele_programarii_c/pasul1/AfisareUrareCraciun/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//#941.În preajma Crăciunului toţi suntem sau redevenim copii.
+//Scrie un program care afişează pe ecran o urare pentru cei dragi ţie!
+int main(){
+    string UrareCraciun;
+  UrareCraciun = "Craciun fericit!";
+   cout<< UrareCraciun;
+    return 0;
+}
