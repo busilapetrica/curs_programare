f18aa131d993592f95b65eb6ab10d84c7c729c95
Petrica <41289735+pet5555@users.noreply.github.com>
2023-04-24 18:44:59 +0300

    Afisare suma a doua numere naturale

---
+++ 00bazele_programarii_c/pasul1/AfisareSumaDouaNumereNaturale/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//#939.Să se scrie un program care citeşte de la tastatura două numere naturale şi determină suma lor.
+int main(){
+    int a,b,SumaNumerelor;
+    cin>> a >> b;
+    SumaNumerelor = a+b;
+    //afisare suma numerelor a+b
+    cout<< SumaNumerelor;
+    return 0;
+}
