e1dcae89538c13eefc7a6609ae1c3b777a012dd6
Petrica <41289735+pet5555@users.noreply.github.com>
2023-04-24 18:20:21 +0300

    adaugare variabila ValuareAdaugataLuiY

--- 00bazele_programarii_c/pasul1/trimplul_lui_y/main.cpp
+++ 00bazele_programarii_c/pasul1/trimplul_lui_y/main.cpp
@@ -3,8 +3,9 @@ using namespace std;
 //#814.Fiind date două numere naturale x și y determinați
 //valoarea care trebuie adunată la x pentru a obține triplul lui y.
 int main(){
-    int x,y;
+    int x,y, ValuareAdaugataLuiY;
     cin>>x >>y;
-    cout<< y*3-x;
+    ValuareAdaugataLuiY = y*3-x;
+    cout<< ValuareAdaugataLuiY;
     return 0;
 }
