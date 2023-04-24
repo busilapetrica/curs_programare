3e9cef702cfca5627d6ffa24b1d3878eee096854
Petrica <41289735+pet5555@users.noreply.github.com>
2023-04-24 20:15:35 +0300

    afisare radacina patrata a lui n

---
+++ 00bazele_programarii_c/pasul1/Radacina patrata a lui N/main.cpp
@@ -0,0 +1,12 @@
+//Se dă un număr real n. Să se afișeze rădăcina sa pătrată.
+//#1343 radical1
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main(){
+   double n, RadacinaSaPatrata;
+   cin>> n;
+   RadacinaSaPatrata = sqrt(n);
+   cout << RadacinaSaPatrata;
+    return 0;
+}
