3c160c2afb10fca3e62d1506018e70245ec479aa
Petrica <41289735+pet5555@users.noreply.github.com>
2023-04-24 18:57:02 +0300

    adaugare problema calcul diferenta a doua numere naturale

---
+++ 00bazele_programarii_c/pasul1/Calcul diferenta a doua numere naturale/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//#1258.Se dau 2 numere naturale. Calculați diferenţa lor.
+ int main(){
+   int a,b, DiferentaNumere;
+   a=4;
+   b=2;
+   DiferentaNumere = a-b;
+   //afisare diferenta numere
+   cout<< DiferentaDumere;
+   return 0;
+}
