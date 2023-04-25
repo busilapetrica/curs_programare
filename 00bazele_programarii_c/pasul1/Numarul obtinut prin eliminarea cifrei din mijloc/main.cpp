3790448e40a2b3d3017093927d53019e51a5f620
Petrica Busila <pentrica.busila@elmbv>
2023-04-25 16:26:42 +0300

    Numarul obtinut prin eliminarea cifrei din mijloc

---
+++ 00bazele_programarii_c/pasul1/Numarul obtinut prin eliminarea cifrei din mijloc/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*Sa se scrie un program care citeste de la tastatura un numar
+natural cu exact trei cifre si determina numarul obtinut prin eliminarea cifrei din mijloc.*/
+int main (){
+  int numarIntrare, nr1, nr3, nrConcatenat;
+  cout << "Introdu un numar de trei cifre intre 100 si 1000" << endl;
+  cin >> numarIntrare;
+  if (numarIntrare < 100 || numarIntrare >= 1000)
+    {
+      cout << "Numarul introdus nu se afla in range-ul 100-1000" << endl;
+      }else{
+       nr1 = (numarIntrare/100)*10;
+        nr3 = numarIntrare%10;
+      // Concatenare 
+      nrConcatenat = nr1+nr3;
+    }
+  cout << nrConcatenat;
+  return 0;
+}
