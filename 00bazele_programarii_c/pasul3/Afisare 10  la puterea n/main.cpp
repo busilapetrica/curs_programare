97a3414c42ba286d51960b1d85b370243e3284c9
Petrica Busila <pentrica.busila@elmbv>
2023-06-12 12:34:21 +0300

    Afisare 10  la puterea n

---
+++ 00bazele_programarii_c/pasul3/Afisare 10  la puterea n/main.cpp
@@ -0,0 +1,14 @@
+/*
+e dă un număr natural n.
+Afișați în ordine crescătoare numerele naturale pare nenule mai mici sau egale cu n.
+*/
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cin >> n;
+  cout << 1;
+  for (int i = 1; i <= n; ++i)
+    cout << 0;
+  return 0;
+}
