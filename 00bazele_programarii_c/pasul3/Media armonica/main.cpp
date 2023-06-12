fb03b1a10208966ce4da331ffa7a21319eaff1d0
Petrica Busila <pentrica.busila@elmbv>
2023-06-12 13:42:00 +0300

    Media armonica

---
+++ 00bazele_programarii_c/pasul3/Media armonica/main.cpp
@@ -0,0 +1,17 @@
+/*
+Se dau n numere naturale. Calculați media armonică
+a celor n numere citite cu 2 zecimale exacte.
+*/
+	#include <iostream>
+	#include <iomanip>
+	using namespace std;
+	int main() {
+	    double number1, number2, s = 0;
+	    cin >> number1; // primul numar(5)
+	    for (int i = 1; i <= number1; i++) {
+	        cin >> number2; //ia al doilea rand (1 -> 5 )
+	        s = s + 1 / number2;
+	    }
+	    s = number1 / s;
+	    cout << fixed << setprecision(2) << s;
+	}
