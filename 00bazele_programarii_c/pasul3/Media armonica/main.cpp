409460fda102a9b370f38e5959f3c0d000ceaadf
Petrica <41289735+pet5555@users.noreply.github.com>
2023-06-25 18:30:19 +0300

    modificare problema media armonicii

--- 00bazele_programarii_c/pasul3/Media armonica/main.cpp
+++ 00bazele_programarii_c/pasul3/Media armonica/main.cpp
@@ -5,17 +5,19 @@ EX:
 	5
 1 2 3 4 5
 */
-*/
 	#include <iostream>
-	#include <iomanip>
-	using namespace std;
-	int main() {
-	    double number1, number2, s = 0;
-	    cin >> number1; // primul numar(5)
-	    for (int i = 1; i <= number1; i++) {
-	        cin >> number2; //ia al doilea rand (1 -> 5 )
-	        s = s + 1 / number2;
-	    }
-	    s = number1 / s;
-	    cout << fixed << setprecision(2) << s;
-	}
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double number1, number2, suma_armonica = 0;
+    cin >> number1; // primul numar (5)
+
+    for (int i = 1; i <= number1; i++) {
+        cin >> number2; // ia al doilea rand (1 -> 5)
+        suma_armonica = suma_armonica + 1 / number2;
+    }
+
+    double media_armonica = number1 / suma_armonica;
+    cout << fixed << setprecision(2) << media_armonica;
+}
